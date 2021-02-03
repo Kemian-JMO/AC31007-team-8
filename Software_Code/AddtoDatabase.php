f90bfc92f7b8407a36ba9dd558a19f8548286188
Kemian-JMO <55388741+Kemian-JMO@users.noreply.github.com>
2021-02-03 10:55:02 +0000

    Create AddtoDatabase.php

---
+++ Software_Code/AddtoDatabase.php
@@ -0,0 +1,45 @@
+<?php
+session_start();
+if ($_SESSION["loggedIn"] != "true") {
+    header('Location: http://oai-content.co.uk');
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>baaam</title>
+  </head>
+  <body>
+    <p>hi</p>
+
+    <?php
+
+    if (isset($_POST['submit']))
+    {
+      $test = $_POST['testing'];
+      if ($_POST['formQuestion']) {
+
+      	foreach ( $_POST['formQuestion'] as $key=>$value ) {
+
+          var_dump($_POST['formQuestion']);
+
+      	//$values = mysql_real_escape_string($value);
+      	//$query = mysql_query("INSERT INTO my_hobbies (hobbies) VALUES ('$values')", $connection );
+
+      	}
+
+      	}
+      var_dump($_POST['testing']);
+    }
+
+
+
+
+
+     ?>
+
+     <p>hih</p>
+  </body>
+</html>
